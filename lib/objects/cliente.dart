74d702e4a47a0c442bcea2a8d86014de391470aa
marcelom <marcemolas@gmail.com>
2021-11-28 16:33:04 -0300

    genere nomas la clase dart con la extension

--- lib/objects/cliente.dart
+++ lib/objects/cliente.dart
@@ -1,14 +1,58 @@
+import 'dart:convert';
+
 class Cliente {
   String? ruc;
   String? nombre;
   String? email;
 
-  Cliente({
-    this.ruc,
-    this.nombre,
-    this.email
-  });
+  Cliente({this.ruc, this.nombre, this.email});
 
+  Cliente copyWith({
+    String? ruc,
+    String? nombre,
+    String? email,
+  }) {
+    return Cliente(
+      ruc: ruc ?? this.ruc,
+      nombre: nombre ?? this.nombre,
+      email: email ?? this.email,
+    );
+  }
 
+  Map<String, dynamic> toMap() {
+    return {
+      'ruc': ruc,
+      'nombre': nombre,
+      'email': email,
+    };
+  }
 
+  factory Cliente.fromMap(Map<String, dynamic> map) {
+    return Cliente(
+      ruc: map['ruc'] != null ? map['ruc'] : null,
+      nombre: map['nombre'] != null ? map['nombre'] : null,
+      email: map['email'] != null ? map['email'] : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Cliente.fromJson(String source) =>
+      Cliente.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'Cliente(ruc: $ruc, nombre: $nombre, email: $email)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Cliente &&
+        other.ruc == ruc &&
+        other.nombre == nombre &&
+        other.email == email;
+  }
+
+  @override
+  int get hashCode => ruc.hashCode ^ nombre.hashCode ^ email.hashCode;
 }
--- lib/objects/producto.dart
+++ lib/objects/producto.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Producto {
   int? codigo;
   String? nombre;
@@ -12,7 +14,7 @@ class Producto {
   });
 
   @override
-  bool operator == (Object other) {
+  bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
     return other is Producto &&
@@ -24,7 +26,52 @@ class Producto {
 
   @override
   // TODO: implement hashCode
-  int get hashCode => super.hashCode;
+  int get hashCode {
+    return codigo.hashCode ^
+        nombre.hashCode ^
+        precio.hashCode ^
+        existencia.hashCode;
+  }
 
+  Producto copyWith({
+    int? codigo,
+    String? nombre,
+    int? precio,
+    int? existencia,
+  }) {
+    return Producto(
+      codigo: codigo ?? this.codigo,
+      nombre: nombre ?? this.nombre,
+      precio: precio ?? this.precio,
+      existencia: existencia ?? this.existencia,
+    );
+  }
 
+  Map<String, dynamic> toMap() {
+    return {
+      'codigo': codigo,
+      'nombre': nombre,
+      'precio': precio,
+      'existencia': existencia,
+    };
+  }
+
+  factory Producto.fromMap(Map<String, dynamic> map) {
+    return Producto(
+      codigo: map['codigo'] != null ? map['codigo'] : null,
+      nombre: map['nombre'] != null ? map['nombre'] : null,
+      precio: map['precio'] != null ? map['precio'] : null,
+      existencia: map['existencia'] != null ? map['existencia'] : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Producto.fromJson(String source) =>
+      Producto.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Producto(codigo: $codigo, nombre: $nombre, precio: $precio, existencia: $existencia)';
+  }
 }
